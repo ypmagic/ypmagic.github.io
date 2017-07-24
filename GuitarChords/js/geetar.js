e30c86368dde260aa13d42c9f9949ae3f9537ace
Young Park <young_park@brown.edu>
2017-07-24 21:05:58 +0900

    deleting stuff

--- GuitarChords/js/geetar.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("chords").addEventListener("change", function() {
-	var chord = document.querySelector('option:checked').id;
-	if (chord !== "select") {
-		document.querySelector("#chord").setAttribute("src", "images/" + chord + ".png");
-	}
-});
